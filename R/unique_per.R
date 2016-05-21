b9c7dd39df0e7201e7ed9be8c6bd70fc4986dd43
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-21 16:10:23 -0400

    unique_per function

---
+++ R/unique_per.R
@@ -0,0 +1,16 @@
+#' Utility to check if a data frame column (name) has unique values based on a given unique ID column (uid)
+#'
+#' @param d Data frame
+#' @param name Quoted column name
+#' @param uid Quoted column name of unique id
+unique_per <- function(d, name, uid = names(d)[1]) {
+
+  multiples <- dplyr::filter(dplyr::count_(dplyr::distinct(dplyr::select_(d, uid, name)), uid), n > 1)
+
+  if(nrow(muliples) > 0) {
+    warning(deparse(substitute(d)), " columns ", name, " is not unique per ", uid)
+    return(multiples)
+  } else {
+    return(TRUE)
+  }
+}
